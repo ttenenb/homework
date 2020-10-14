f0b543ebed3794daa40d28177d4448fa8b2ea5e0
ttenenbaum <tenenbaumtzvi@gmail.com>
2020-10-14 14:20:14 -0400

    Homework 69

--- js/69/geolocation.js
+++ js/69/geolocation.js
@@ -22,19 +22,16 @@
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=${myKey}&units=${units}`)
             .then(r => r.json())
             .then(weatherData => {
-                console.log(weatherData);
                 loc.text(`The weather in ${weatherData.name}`);
                 weatherIcon.attr('src', `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`);
                 temp.text(`${Math.round(weatherData.main.temp)} degrees and ${weatherData.weather[0].description}`);
                 wind.text(`The wind is at ${weatherData.wind.speed} mph`);
                 let sunrise = new Date(weatherData.sys.sunrise * 1000);
-                sunrise.setHours(sunrise.getUTCHours() - sunrise.getTimezoneOffset()/60 + 1);
                 let sunset = new Date(weatherData.sys.sunset * 1000);
-                sunset.setHours(sunset.getUTCHours() - sunset.getTimezoneOffset() / 60 + 1);
                 sunIcon.attr('src', 'img/sun.jpg');
-                sun.html(`Sunrise : ${sunrise.toLocaleTimeString()}`);
+                sun.html(`Sunrise - ${sunrise.toLocaleTimeString()}`);
                 sun.append('<br/>');
-                sun.append(` Sunset : ${ sunset.toLocaleTimeString() }`);
+                sun.append(` Sunset - ${ sunset.toLocaleTimeString() }`);
             })
             .catch(e => console.error(e));
 
