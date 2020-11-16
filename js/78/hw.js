e942019760be36e804585c25ab607e4b223fb8c2
ttenenbaum <tenenbaumtzvi@gmail.com>
2020-11-16 11:49:21 -0500

    Homework 78

---
+++ js/78/hw.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    $(document.body).append(`
+    <ul>
+        <li>
+            One 
+        </li>
+        <li>
+            Two
+        </li> 
+        <li>
+            Three
+        </li>
+    </ul>
+    `);
+}());
