a935265006a363ddc678d533f11bff37e7668176
ttenenbaum <tenenbaumtzvi@gmail.com>
2020-11-22 00:00:51 -0500

    Homework 79

---
+++ js/79/src/index.js
@@ -0,0 +1,18 @@
+ import $ from 'jquery';
+// (function () {
+//     'use strict';
+
+    $(document.body).append(`
+    <ul>
+        <li>
+            One 
+        </li>
+        <li>
+            Two
+        </li> 
+        <li>
+            Three
+        </li>
+    </ul>
+    `);
+// }());
